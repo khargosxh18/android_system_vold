e8a6d793b09ca78ee0e177f1f6017588d2bc2567
Satya Tangirala <satyat@google.com>
2021-06-08 12:55:37 -0700

    Don't export storeKey(), and update comments
    storeKey() is no longer used outside KeyStorage.cpp, so make it a static
    function.  Also fix the documentation for storeKey() (e.g. it's no
    longer safe to directly move/rename directories created by storeKey() --
    one must use RenameKeyDir() instead).
    
    No functional changes.
    
    [ebiggers@ - cleaned up slightly from satyat@'s original change]
    
    Bug: 190398249
    Change-Id: I85918359e77bef414dfddfe5ded30fcde6514013

--- KeyStorage.h
+++ KeyStorage.h
@@ -41,12 +41,6 @@ extern const KeyAuthentication kEmptyAuthentication;
 bool createSecdiscardable(const std::string& path, std::string* hash);
 bool readSecdiscardable(const std::string& path, std::string* hash);
 
-// Create a directory at the named path, and store "key" in it,
-// in such a way that it can only be retrieved via Keymaster and
-// can be securely deleted.
-// It's safe to move/rename the directory after creation.
-bool storeKey(const std::string& dir, const KeyAuthentication& auth, const KeyBuffer& key);
-
 // Create a directory at the named path, and store "key" in it as storeKey
 // This version creates the key in "tmp_path" then atomically renames "tmp_path"
 // to "key_path" thereby ensuring that the key is either stored entirely or
