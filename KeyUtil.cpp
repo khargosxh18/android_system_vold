0d2f682cf37f234d78bb54c25614175bf77174b5
Eric Biggers <ebiggers@google.com>
2023-08-11 20:25:09 +0000

    vold: Revert "Introduce ro.crypto.uses_fs_ioc_add_encryption_key property"
    This reverts commit 78f806198f85ca37abb617235c89abaca7d1c19c.
    
    There is no code that reads this system property, logcat already shows
    whether the FS keyring is being used, and all devices launching with
    Android 11 and later are guaranteed to use the FS keyring anyway.
    
    Bug: 154327249
    Change-Id: Id906efedd89d5bcac5370fb141cdbf7848932d95

--- KeyUtil.cpp
+++ KeyUtil.cpp
@@ -28,7 +28,6 @@
 
 #include <android-base/file.h>
 #include <android-base/logging.h>
-#include <android-base/properties.h>
 #include <keyutils.h>
 
 #include "KeyStorage.h"
@@ -93,7 +92,6 @@ static bool isFsKeyringSupportedImpl() {
         PLOG(WARNING) << "Unexpected error from FS_IOC_ADD_ENCRYPTION_KEY";
     }
     LOG(INFO) << "Detected support for FS_IOC_ADD_ENCRYPTION_KEY";
-    android::base::SetProperty("ro.crypto.uses_fs_ioc_add_encryption_key", "true");
     return true;
 }
 
